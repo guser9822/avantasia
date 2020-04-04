1772c8bfe8c5c770ad81dd925b783357d739325f
guser9822 <riccardo.daniello13@gmail.com>
2020-04-04 23:03:05 +0200

    Added balanceOf function to RDToken contract

--- src/components/RDToken/index.js
+++ src/components/RDToken/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './RDToken.css'
 
 const EHTER_UNIT_NAME = 'ether';
-const DEFAULT_GAS_LIMIT = 5000000
+const DEFAULT_GAS_LIMIT = 5000000;
 
-const TOTAL_SUPPLY = 'total_supply'
+const TOTAL_SUPPLY = 'total_supply';
+const ACCOUNT_BALANCE = 'account_balance';
+
 export default class RDToken extends React.Component {
 
     constructor(props) {
@@ -22,6 +24,8 @@ export default class RDToken extends React.Component {
                     gasPrice: '20000000000',// default gas price in wei, 20 gwei in this case */
                 }),
             totalSupply: undefined,
+            otherAddress: "",
+            balanceAtAddress: undefined,
         }
     }
 
@@ -29,15 +33,43 @@ export default class RDToken extends React.Component {
 
         switch (operation) {
             case TOTAL_SUPPLY:
-                console.log('Click')
-                this.getTotalSupply(this.state.userAddress, this.state.rdTokenContract)
+                const totSupply = this.getTotalSupply(this.state.userAddress, this.state.rdTokenContract)
+                this.setState({
+                    totalSupply: totSupply
+                })
+                break
+            case ACCOUNT_BALANCE:
+                const addrBal = this.getBalanceByAddress(this.state.userAddress, this.state.otherAddress, this.state.rdTokenContract);
+                this.setState({
+                    balanceAtAddress: addrBal
+                })
                 break
         }
     }
 
+    getBalanceByAddress = (userAddress, address, rdTokenContract) => {
+
+        if (!userAddress || ! address || ! rdTokenContract) {
+            console.error('Error in getTotalSuppy, on or more input parameter is invalid.')
+            return
+        }
+
+        rdTokenContract.methods.
+            balanceOf(address).
+            call({ from: userAddress })
+            .then(balance => {
+                console.log(`The balance for the adddress ${address} is ${balance} RDT`)
+                this.setState({
+                    balanceAtAddress : `( ${balance} RDT)`
+                })
+            }).catch(err => console.error(`Error while invoking totalSupply : `, err))
+
+        return 'gathering....'
+    }
+
     getTotalSupply = (userAddress, rdTokenContract) => {
 
-        if (!userAddress) {
+        if (!userAddress || !rdTokenContract) {
             console.error('Error in getTotalSuppy, on or more input parameter is invalid.')
             return
         }
@@ -50,10 +82,19 @@ export default class RDToken extends React.Component {
                 totalSupply: `( ${res} RDT )`
             })
             return res
-        })
-            .catch(err => console.error(`Error while invoking totalSupply : `, err))
+        }).catch(err => console.error(`Error while invoking totalSupply : `, err))
 
         return '...gathering'
+    }
+
+    isBalanceButtonDisabled = () => {
+        return !this.state.otherAddress || !this.state.otherAddress.length || this.state.otherAddress === ""
+    }
+
+    changeOtherAddressHandle = (event) => {
+        this.setState({
+            otherAddress: event.target.value
+        })
     }
 
     render() {
@@ -61,8 +102,16 @@ export default class RDToken extends React.Component {
             <article>
                 <div className="RDToken">
                     <h3>RDToken {this.state.totalSupply}</h3>
+                    <label>Address : </label>
+                    <input type="text"
+                        value={this.state.otherAddress}
+                        onChange={this.changeOtherAddressHandle}
+                    />
+                    <label className="RDToken-Label">{this.state.balanceAtAddress}</label>
                     <div className="RDToken-Operations">
-                        <button onClick={() => this.onOperationClick(TOTAL_SUPPLY)}>Total Supply</button>
+                        <button onClick={() => this.onOperationClick(TOTAL_SUPPLY)}> Total Supply </button>
+                        <button onClick={() => this.onOperationClick(ACCOUNT_BALANCE)}
+                            disabled={this.isBalanceButtonDisabled()}> Find Balance </button>
                     </div>
                 </div>
             </article>
