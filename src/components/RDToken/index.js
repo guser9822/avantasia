5b875074f37edd3bf60b273d6480fd5230b37fef
guser9822 <riccardo.daniello13@gmail.com>
2020-04-05 11:44:33 +0200

    Added transfer operation to the RDToken contract

--- src/components/RDToken/index.js
+++ src/components/RDToken/index.js
@@ -6,6 +6,7 @@ const DEFAULT_GAS_LIMIT = 5000000;
 
 const TOTAL_SUPPLY = 'total_supply';
 const ACCOUNT_BALANCE = 'account_balance';
+const TRANSFER = 'transfer';
 
 export default class RDToken extends React.Component {
 
@@ -26,6 +27,7 @@ export default class RDToken extends React.Component {
             totalSupply: undefined,
             otherAddress: "",
             balanceAtAddress: undefined,
+            rdTokenUnit: 0,
         }
     }
 
@@ -44,13 +46,52 @@ export default class RDToken extends React.Component {
                     balanceAtAddress: addrBal
                 })
                 break
+            case TRANSFER:
+                this.transferTokens(this.state.userAddress, this.state.otherAddress, this.state.rdTokenContract, this.state.rdTokenUnit);
+                break
         }
+    }
+
+    transferTokens = (userAddress, otherAddress, rdTokenContract, tokenAmount) => {
+
+        if (!userAddress || !otherAddress || !rdTokenContract || !tokenAmount) {
+            console.error('Error in transferTokens, invalid parameters.')
+            return
+        }
+
+        try {
+            console.log(rdTokenContract)
+            rdTokenContract
+                .methods
+                .transfer(otherAddress, tokenAmount).estimateGas({
+                    from: userAddress,
+                }).then(gas => {
+
+                    console.log(`transferTokens gas estimation ${gas}`)
+
+                    rdTokenContract.
+                        methods
+                        .transfer(otherAddress, tokenAmount)
+                        .send({
+                            from: userAddress,
+                            gas,
+                            gasPrice: 200000000000,
+                        }).then(res => {
+                            console.log(`Transferred ${tokenAmount} RDT from ${userAddress} to ${otherAddress} : `, res)
+                        }).catch(err => console.error(`Error while invoking transferTokens : `, err))
+
+                }).catch(err => console.error(`Error while invoking transferTokens gas estimation : `, err))
+
+        } catch (error) {
+            console.error('transferTokens, generic error : ', error)
+        }
+
     }
 
     getBalanceByAddress = (userAddress, address, rdTokenContract) => {
 
         if (!userAddress || !address || !rdTokenContract) {
-            console.error('Error in getTotalSuppy, on or more input parameter is invalid.')
+            console.error('Error in getTotalSuppy, invalid parameters.')
             return
         }
 
@@ -95,7 +136,7 @@ export default class RDToken extends React.Component {
         return '...gathering'
     }
 
-    isBalanceButtonDisabled = () => {
+    isButtonDisabled = () => {
         return !this.state.otherAddress || !this.state.otherAddress.length || this.state.otherAddress === ""
     }
 
@@ -105,21 +146,48 @@ export default class RDToken extends React.Component {
         })
     }
 
+    transferTokenHandle = (event) => {
+        this.setState({
+            rdTokenUnit: event.target.value
+        })
+    }
+
     render() {
         return (
             <article>
                 <div className="RDToken">
                     <h3>RDToken {this.state.totalSupply}</h3>
-                    <label>Address : </label>
-                    <input type="text"
-                        value={this.state.otherAddress}
-                        onChange={this.changeOtherAddressHandle}
-                    />
-                    <label className="RDToken-Label">{this.state.balanceAtAddress}</label>
+
+                    <div className="RDToken-Form">
+
+                        <div className="RDToken-Form-Elem">
+                            <label>Address : </label>
+                            <input type="text"
+                                value={this.state.otherAddress}
+                                onChange={this.changeOtherAddressHandle}
+                            />
+                            <label>{this.state.balanceAtAddress}</label>
+                        </div>
+
+                        <div className="RDToken-Form-Elem">
+                            <label>RDToken units : </label>
+                            <input type="number"
+                                value={this.state.rdTokenUnit}
+                                onChange={this.transferTokenHandle}
+                                min="0"
+                            />
+                        </div>
+
+                    </div>
+
                     <div className="RDToken-Operations">
                         <button onClick={() => this.onOperationClick(TOTAL_SUPPLY)}> Total Supply </button>
+
                         <button onClick={() => this.onOperationClick(ACCOUNT_BALANCE)}
-                            disabled={this.isBalanceButtonDisabled()}> Find Balance </button>
+                            disabled={this.isButtonDisabled()}> Find Balance </button>
+
+                        <button onClick={() => this.onOperationClick(TRANSFER)}
+                            disabled={this.isButtonDisabled()}> Transfer </button>
                     </div>
                 </div>
             </article>
